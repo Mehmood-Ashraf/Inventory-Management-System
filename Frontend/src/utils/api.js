59fd22c818f1e23d8cff569925258e8751fd3a6a
Mehmood M.Ashraf <mehmoodfazlani@gmail.com>
2025-09-22 09:04:07 +0500

    Update gitignore

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL : "https://inventory-management-system-six-kappa.vercel.app/api"
-    // baseURL : "http://localhost:3000/api"
+    baseURL : import.meta.env.VITE_BACKEND_URL
 })
 
 
